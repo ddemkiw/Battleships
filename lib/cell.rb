ef7bf5b6dcce2f833b54f5c503f37f5f47c5dc6b
Marcus de Wilde <Marcus@Marcuss-MacBook-Pro.local>
2014-12-16 14:14:27 +0000

    Addtional cell test and methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,15 @@
 class Cell
 
-def initialize
-  @empty = true
-end 
+	def initialize
+ 		 @empty = true
+	end 
 
-def empty? 
-  @empty
-end
+	def empty? 
+ 		@empty
+	end
 
+	def anchor(ship)
+		@empty = false
+	end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,9 +3,17 @@ require 'cell'
 describe Cell do 
 
   let(:cell) {Cell.new}
+  let(:ship) {double :ship}
 
   it 'should have a value of empty when initialized' do
     expect(cell).to be_empty
   end
+
+  it 'should be full when there is a ship in the cell' do
+  	cell.anchor(ship)
+  	expect(cell).to_not be_empty
+  end
+
+
 
 end 

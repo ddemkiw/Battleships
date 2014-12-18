f479e6e3407d7b11d442f024a853e9451e058eb6
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-18 20:33:28 +0000

    Playing with attack functionality in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,24 +1,49 @@
 require 'set'
+require_relative 'ship'
+require_relative 'game'
 
 class Board
 
-attr_accessor :attacks, :ships
+attr_accessor :attacks, :ships, :fleet
+
+BATTLE = Ship.battleship
+SUB = Ship.submarine
 
 	def initialize
 		@attacks = Set[]
+		@fleet = Set[]
 		@ships = Set[]
+
 	end
 
 	def attack(coordinates) 
+		raise 'You may not attack the same cell twice, please try again' if @attacks.include? coordinates
+		case self.hit?
+			when BATTLE.coordinates.include?(coordinates)
+				BATTLE.hit!
+			when SUB.coordinates.include?(coordinates)
+				SUB.hit!
+			else 
+				p "wooo"
+			end
 		@attacks << coordinates
 	end
 
-	def place_ship(ship)
-		@ships << ship
-		@ships.flatten!
+	def build_fleet(ship)
+		raise 'You may place two ships in the same cell, please try again' if @fleet.intersect?(ship)
+		@fleet << ship
+		@fleet.flatten!
 	end
 
 	def hit?
-		@attacks.intersect?(@ships) 
+		@attacks.intersect?(@fleet)
+	end
+
+	def hit!
+
+	end
+
+	def game_over? 
+		@attacks.superset?(@fleet)
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,39 +1,68 @@
 require 'board'
+require 'ship'
+require 'game'
 
 	describe Board do 
 		
 		let(:board) {Board.new}
+		let(:set1) {Set[:E5, :B6, :C3]}
+		let(:set2) {Set[:D3, :D4, :D5]}
 
-		it 'is created with an empty array of attacks' do
+
+		it 'is created with an empty set of attacks' do
 			expect(board.attacks).to be_empty
 		end
 
-		it 'is created with an empty array of ships' do
+		it 'is created with an empty set of ships' do
 			expect(board.ships).to be_empty
 		end
 
-		it 'when an attack takes place its coordinates are stored in the attack array' do
+		it 'battleship should receive hit! when attack hits battleship' do 
+			Battle = Ship.battleship
+			coord= Set[:A3, :A4, :A5]
+			Battle.coordinates << coord
+			Battle.coordinates.flatten!
+			expect(Battle.hits).to eq(0)
+			board.attack(:A3)
+			expect(Battle.hits).to eq(1)
+		end
+
+		it 'should store coordinates when when an attack takes place' do
 			board.attack(:E5)
 			expect(board.attacks).to include (:E5)
 		end
 		
-		it 'should be able to place a ship array' do
-			board.place_ship([:E5, :B6, :C3])
-			board.place_ship( [:D3, :D4, :D5])
-			expect(board.ships).to include([:E5, :B6, :C3] )
-			expect(board.ships).to include([:D3, :D4, :D5] )
+		it 'should be able to build a fleet set' do
+			board.build_fleet(set1)
+			board.build_fleet( set2)
+			expect(board.fleet).to include(:E5)
+			expect(board.fleet).to include(:D3)
 		end
 
 		it 'should know when an attack\'s coordinates matches a ship' do
-			set = Set[1,2,3,4]
-			board.place_ship(set)
-			board.attack(4)
+			board.build_fleet(set1)
+			board.attack(:E5)
 			expect(board).to be_hit
 		end
 
 		it 'should not allow the same coordinate to be attacked twice' do
 			board.attack(1)
-			expect({board.attack(1)}).to to raise_error('You may not attack the same cell twice, please try again') 
+			expect(lambda{board.attack(1)}).to raise_error('You may not attack the same cell twice, please try again') 
 		end
+
+		it 'should not allow ships to overlap' do
+			board.build_fleet(set1)
+			set3 = Set[:E5, :E6, :E7]
+			expect(lambda{board.build_fleet(set3)}).to raise_error('You may place two ships in the same cell, please try again') 
+		end
+
+		it 'should know when a game is over' do
+			board.build_fleet(set1)
+			board.attack(:E5)
+			board.attack(:B6)
+			board.attack(:C3)
+			expect(board).to be_game_over
+		end
+
 
 	end

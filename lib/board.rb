b3d0b0c14b74de4885c31ffee217d27cd444bec1
Marcus de Wilde <Marcus@Marcuss-MacBook-Pro.local>
2014-12-18 15:27:54 +0000

    Board working for receiving attack coordinates and placing ships

--- lib/board.rb
+++ lib/board.rb
@@ -1,34 +1,19 @@
-require_relative 'ship'
-require_relative 'water'
-require 'set'
+class Board
 
-	class Board
+attr_accessor :attacks, :ships
 
-		attr_accessor :ships, :attacks, :coordinates, :contents
-
-		def initialize
-			@ships = Set.new
-			@attacks = Set.new
-			# coordinates = @coordinates
-		 end
-
-		def place_ship(coordinates)
-			raise 'You may not attack the same cell twice, please try again' if ships.include?(coordinates.to_sym)
-			# @contents.hit!
-		 	@ships <<(coordinates.to_sym)
-		 end
-
-		def attack_cell(coordinates)
-		 	raise 'You may not attack the same cell twice, please try again' if attacks.include?(coordinates.to_sym)
-			# @contents.hit!
-		 	@attacks <<(coordinates.to_sym)
-		 end
-
-		def hit?
-    	 	return @contents
-   		 end
-
-		def game_over?
-		 	@ships >= @attacks
-		 end
+	def initialize
+		@attacks = []
+		@ships = []
 	end
+
+	def attack(coordinates) 
+		@attacks << coordinates
+	end
+
+	def place_ship(ship)
+		@ships << ship
+	end
+
+
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,36 +1,32 @@
 require 'board'
 
-
-
 	describe Board do 
-
-		let(:player) {double :player} 
+		
 		let(:board) {Board.new}
 
-		it 'is initialized with a empty set of ships' do
-			expect(board.ships).to be_a_kind_of Set
+		it 'is created with an empty array of attacks' do
+			expect(board.attacks).to be_empty
 		end
 
-		it 'is initialized with a empty set of attacks' do
-			expect(board.attacks).to be_a_kind_of Set
+		it 'is created with an empty array of ships' do
+			expect(board.ships).to be_empty
 		end
 
-		it 'should accept coordinates for ships' do
-			board.place_ship("E5")
-			expect(board.ships).to include :E5
-		end
-
-		it 'should accept coordinates for attacks' do
-			board.attack_cell("E5")
-			expect(board.attacks).to include :E5
+		it 'when an attack takes place its coordinates are stored in the attack array' do
+			board.attack(:E5)
+			expect(board.attacks).to include (:E5)
 		end
 		
-		it 'should not be able to attack same coordinates twice' do
-      	    board.attack_cell("E5")
-      		expect(lambda{board.attack_cell("E5")}).to raise_error('You may not attack the same cell twice, please try again') 
-  		end
+		it 'should be able to place a ship array' do
+			board.place_ship([:E5, :B6, :C3])
+			board.place_ship( [:D3, :D4, :D5])
+			expect(board.ships).to include([:E5, :B6, :C3] )
+			expect(board.ships).to include([:D3, :D4, :D5] )
+		end
 
-		it 'should know if the game is over' do 
+		it 'should know when an attack\'s coordinates matches a ship' do
+			board.place_ship([:D3, :D4, :D5])
+
 		end
 
 	end
